5d9f0238828957629a6c06788b8195fbc813eb75
ChillVibezTechHub <github@chillvibeztech.com>
2026-02-06 16:05:50 -0500

    Update MonitoringModule_Setup.ps1

--- MonitoringModule/MonitoringModule_Setup.ps1
+++ MonitoringModule/MonitoringModule_Setup.ps1
@@ -1,1 +1,6 @@
+$wiresharkfilepath = Get-Childitem –Path $Env:USERPROFILE\Downloads\Wireshark*
+
+Enable-VMIntegrationService -Name "Guest Service Interface" -VMName "Router"
+
+Copy-VMFile -Name "Router" -SourcePath $wiresharkfilepath -DestinationPath "C:\LabFiles\WiresharkInstall.exe" -CreateFullPath -FileSource Host
 
